e7c80aad58d1c219e7e32c93a136fbfc9c3c7997
OmegaRed767 <omegared767@gmail.com>
2021-07-17 22:28:39 +0530

    handeled error

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ const Message = require("./Api/Routes/Message");
 app.use("/User", User);
 app.use("/Message", Message);
 
+// handel error
+app.use((req, res, next) => {
+  const error = new Error("Uh Ho Error Occured");
+  error.status = 500;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(500 || error.status);
+  res.json({ msg: error.message });
+});
+
 module.exports = app;
