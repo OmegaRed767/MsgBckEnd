92529367ff5b7c6086129fbe82c71e9490f5201a
OmegaRed767 <omegared767@gmail.com>
2021-07-21 20:28:07 +0530

    SignUp Login Added

--- Api/Models/User.js
+++ Api/Models/User.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema(
   {
     _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, min: 4, max: 10 },
+    img: { type: String },
     email: {
       type: String,
       reqired: true,
--- Api/Routes/User.js
+++ Api/Routes/User.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const routes = express.Router();
+// Mongoose
+const mongoose = require("mongoose");
+// UserSchema
+const User = require("../Models/User");
+// BcryptJS
+const bcrypt = require("bcryptjs");
 
 routes.get("/get", (req, res, next) => {
   res.status(200).json({
@@ -14,15 +20,61 @@ routes.get("/get/:Id", (req, res, next) => {
   });
 });
 
-routes.post("/post", (req, res, next) => {
-  const info = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-  res.status(200).json({
-    msg: "Post handeled",
-    info,
-  });
+routes.post("/SignUp", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((doc) => {
+      if (doc.length > 0) {
+        return res.status(404).json({ msg: "user exists" });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(200).json(err);
+          }
+          if (hash) {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((val) => {
+                return res.status(200).json(val);
+              })
+              .catch((Err) => {
+                return res.status(400).json(Err);
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ msg: err });
+    });
+});
+
+routes.post("/Login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((doc) => {
+      if (doc.length <= 0) {
+        return res.status(404).json({ msg: "User Dosent Exists" });
+      } else {
+        bcrypt.compare(req.body.password, doc[0].password, (err, hash) => {
+          if (err) {
+            return res.status(200).json(err);
+          }
+          if (hash) {
+            return res.status(404).json({ msg: "login Successfull" });
+          }
+          return res.status(400).json({ msg: "login unsuccessful" });
+        });
+      }
+    })
+    .catch((Err) => {
+      return res.status(400).json(Err);
+    });
 });
 
 routes.patch("/patch/Id", (req, res, next) => {
