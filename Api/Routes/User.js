dafc38b923cadccf7ec4a929232313fe9b0d8220
OmegaRed767 <omegared767@gmail.com>
2021-07-25 18:12:39 +0530

    Basic image upload to own server

--- Api/Routes/User.js
+++ Api/Routes/User.js
@@ -6,6 +6,33 @@ const mongoose = require("mongoose");
 const User = require("../Models/User");
 // BcryptJS
 const bcrypt = require("bcryptjs");
+// Multer
+const multer = require("multer");
+
+//storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./Uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname + "-" + Date.now() + ".png");
+  },
+});
+
+//fileFilter
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "file.png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const Upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
 
 routes.get("/get", (req, res, next) => {
   res.status(200).json({
@@ -18,6 +45,31 @@ routes.get("/get/:Id", (req, res, next) => {
     msg: "get handeled",
     _id: req.params.Id,
   });
+});
+
+routes.patch("/info/:Id", Upload.single("Image"), (req, res, next) => {
+  User.find({ _id: req.params.Id })
+    .exec()
+    .then((doc) => {
+      if (doc.length === 0) {
+        return res.status(400).json({ msg: "Dosent exists" });
+      }
+      const info = {
+        img: req.file.originalname,
+        name: req.body.name,
+      };
+      User.updateOne({ _id: req.params.Id }, { $set: info })
+        .exec()
+        .then((val) => {
+          return res.status(200).json(val);
+        })
+        .catch((err) => {
+          return res.status(200).json(err);
+        });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 routes.post("/SignUp", (req, res, next) => {
